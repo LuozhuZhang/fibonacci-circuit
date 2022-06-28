3f51cb04bccc338d8a4d7933fdbb2dba7ff4ad3e
Mickey <hedgefund996@gmail.com>
2022-06-28 17:23:25 +0800

    Config && define columns

--- src/example1.rs
+++ src/example1.rs
@@ -1,3 +1,24 @@
+use std::marker::PhantomData;
+
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    circuit::*,
+    plonk::*,
+};
+
+#[derive(Debug, Clone)]
+// * 1.Config
+struct FiboConfig {
+    // 在这里定义advice column的数量
+    pub advice: [Column<Advice>; 3],
+    pub selector: Selector,
+}
+
+struct FiboChip<F: FieldExt> {
+    config: FiboConfig,
+    _marker: PhantomData<F>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
